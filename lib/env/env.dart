c7245af9ade781f9709931b3ad6241ae37711563
Furkan Topaloğlu <furkan@berkut.tech>
2023-11-24 02:10:12 +0300

    Added envied package for Google maps api key.

---
+++ lib/env/env.dart
@@ -0,0 +1,10 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'TOKEN', obfuscate: true)
+  static const token = _Env.token;
+}
